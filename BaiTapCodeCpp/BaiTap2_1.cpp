8482268ce6ab3315a68f5d22443c12ebb6548d20
nvminh09 <nvminh09@gmail.com>
2024-02-19 02:16:39 +0700

    Create exercise 2.2 and 2.3

--- BaiTapCodeCpp/BaiTap2_1.cpp
+++ BaiTapCodeCpp/BaiTap2_1.cpp
@@ -1,50 +1,50 @@
-﻿#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
-using namespace std;
-
-enum LuaChon { DUNG, TOM, CUA, BAU, CA, GA, NAI, RUNG };
-const string tenLuaChon[] = { "Dung", "Tom", "Cua", "Bau", "Ca", "Ga", "Nai", "Rung" };
-
-int main() {
-    // Tiền cược ban đầu bằng 0
-    int cuoc[8] = { 0 };
-    // Số lần xuất hiện của con vật ban đầu bằng 0
-    int lanLap[8] = { 0 };
-    // Tiền cược tổng cộng ban đầu bằng 0
-    int chon, cuocTong = 0;
-    srand(time(nullptr));
-
-    while (true) {
-        cout << "Ban chon (0-Dung, 1-Tom, 2-Cua, 3-Bau, 4-Ca, 5-Ga, 6-Nai, 7-Rung): ";
-        cin >> chon;
-
-        if (chon == DUNG) {
-            cout << "Chuc mung nam moi!" << endl;
-            return 0;
-        }
-        if (chon >= TOM && chon <= NAI){
-            cout << "Ban dat " << tenLuaChon[chon] << " bao nhieu tien? ";
-            cin >> cuoc[chon];
-            cuocTong = cuocTong + cuoc[chon];
-        }
-        else if (chon == RUNG) {
-            cout << "Ban da dat cuoc " << cuocTong << "$" << endl;
-            cout << "Ket qua rung: ";
-            for (int i = 0; i < 3; ++i) {
-                int t = rand() % 6 + TOM;
-                lanLap[t]++; // Số lần con vật xuất hiện
-                cout << tenLuaChon[t] << " ";
-            }
-            for (int i = 1; i <= NAI; ++i) {
-                // Nếu con vật xuất hiện với số lần là "lanLap" thì tiền cược nhân với "lanLap" 
-                // Nếu con vật không xuất hiện tiền cược
-                cuocTong += lanLap[i] > 0 ? lanLap[i] * cuoc[i] : -cuoc[i];
-                lanLap[i] = 0;
-            }
-            cout << endl;
-            cout << "Ban con " << cuocTong << "$" << endl;
-        }        
-    }
-}
+﻿//#include <iostream>
+//#include <cstdlib>
+//#include <ctime>
+//
+//using namespace std;
+//
+//enum LuaChon { DUNG, TOM, CUA, BAU, CA, GA, NAI, RUNG };
+//const string tenLuaChon[] = { "Dung", "Tom", "Cua", "Bau", "Ca", "Ga", "Nai", "Rung" };
+//
+//int main() {
+//    // Tiền cược ban đầu bằng 0
+//    int cuoc[8] = { 0 };
+//    // Số lần xuất hiện của con vật ban đầu bằng 0
+//    int lanLap[8] = { 0 };
+//    // Tiền cược tổng cộng ban đầu bằng 0
+//    int chon, cuocTong = 0;
+//    srand(time(nullptr));
+//
+//    while (true) {
+//        cout << "Ban chon (0-Dung, 1-Tom, 2-Cua, 3-Bau, 4-Ca, 5-Ga, 6-Nai, 7-Rung): ";
+//        cin >> chon;
+//
+//        if (chon == DUNG) {
+//            cout << "Chuc mung nam moi!" << endl;
+//            return 0;
+//        }
+//        if (chon >= TOM && chon <= NAI){
+//            cout << "Ban dat " << tenLuaChon[chon] << " bao nhieu tien? ";
+//            cin >> cuoc[chon];
+//            cuocTong = cuocTong + cuoc[chon];
+//        }
+//        else if (chon == RUNG) {
+//            cout << "Ban da dat cuoc " << cuocTong << "$" << endl;
+//            cout << "Ket qua rung: ";
+//            for (int i = 0; i < 3; ++i) {
+//                int t = rand() % 6 + TOM;
+//                lanLap[t]++; // Số lần con vật xuất hiện
+//                cout << tenLuaChon[t] << " ";
+//            }
+//            for (int i = 1; i <= NAI; ++i) {
+//                // Nếu con vật xuất hiện với số lần là "lanLap" thì tiền cược nhân với "lanLap" 
+//                // Nếu con vật không xuất hiện tiền cược
+//                cuocTong += lanLap[i] > 0 ? lanLap[i] * cuoc[i] : -cuoc[i];
+//                lanLap[i] = 0;
+//            }
+//            cout << endl;
+//            cout << "Ban con " << cuocTong << "$" << endl;
+//        }        
+//    }
+//}
---
+++ BaiTapCodeCpp/BaiTap2_2.cpp
@@ -0,0 +1,80 @@
+﻿//#include <iostream>
+//#include <stdio.h>
+//using namespace std;
+//
+//int n, m;
+//int** matran;
+//// int** Matran là mảng được cấp phát động, không cần khai báo kích thước ma trận.
+//
+//// Hàm in ma trận được nhập vào.
+//void InMatran(int** matran, int row, int col) {
+//    for (int i = 0; i < row; i++) {
+//        for (int j = 0; j < col; j++) {
+//            cout << matran[i][j] << "\t";
+//        }
+//        cout << endl;
+//    }
+//}
+//
+//void NhapMatran() {
+//    cout << "Nhap so hang cua ma tran: ";
+//    cin >> n;
+//    cout << "Nhap so cot cua ma tran: ";
+//    cin >> m;
+//    // Tạo ma trận
+//    matran = new int* [n];
+//    for (int i = 0; i < n; i++) {
+//        matran[i] = new int[m];
+//    }
+//    // Nhập giá trị của ma trận
+//    cout << "Nhap phan tu :" << endl;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            cout << "Phan tu [" << i << "][" << j << "] = ";
+//            cin >> matran[i][j];
+//        }
+//    }
+//    // Xuất ma trận được nhập vào.
+//    cout << "Ma tran duoc nhap vao :" << endl;
+//    InMatran(matran, n, m);
+//}
+//// Hàm kiểm tra phần tử tại hàng i cột j có phải là điểm yên hay không.
+//bool KiemTraDiemYen(int** matran, int row, int col, int i, int j) {
+//    for (int x = 0; x < col; x++) {
+//        if (matran[i][j] > matran[i][x])
+//            return false;
+//    }
+//
+//    for (int y = 0; y < row; y++) {
+//        if (matran[i][j] < matran[y][j])
+//            return false;
+//    }
+//
+//    return true;
+//}
+//
+//void XuatDiemYen(int** matran) {
+//    // Xuất điểm yên của ma trận.
+//    int soDiemYen = 0;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            if (KiemTraDiemYen(matran, n, m, i, j)) {
+//                cout << "Diem yen la :" << matran[i][j] << " tai [" << i << "][" << j << "]" << endl;
+//                soDiemYen++;
+//            }
+//        }
+//    }
+//    cout << "Tong so diem yen la :" << soDiemYen << endl;
+//}
+//
+//int main() {
+//    NhapMatran();   
+//    XuatDiemYen(matran);    
+//    // Xóa bỏ vùng nhớ đã được cấp phát cho ma trận.
+//    for (int i = 0; i < n; i++) {
+//        delete[] matran[i];
+//    }
+//    delete[] matran;
+//
+//    return 0;
+//}
